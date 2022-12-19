828f2a5fef1319169bfe30530e352bf0a81fbcae
macmini <u19b138k@wako.ac.jp>
2022-12-19 13:12:56 +0900

    add: authenticationsモデルの作成#26

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_19_035457) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_19_041156) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "authentications", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.integer "uid"
+    t.string "provider"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_authentications_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -27,4 +36,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_19_035457) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "authentications", "users"
 end
