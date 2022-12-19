be4c48d31e373ae8f16c1abbf90b59265f14cd8d
macmini <u19b138k@wako.ac.jp>
2022-12-19 15:17:23 +0900

    add: schedulesモデルの作成#26

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_19_041156) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_19_061605) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,15 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_19_041156) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_authentications_on_user_id"
+  end
+
+  create_table "schedules", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "name"
+    t.integer "assumed_number_people"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_schedules_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -37,4 +46,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_19_041156) do
   end
 
   add_foreign_key "authentications", "users"
+  add_foreign_key "schedules", "users"
 end
