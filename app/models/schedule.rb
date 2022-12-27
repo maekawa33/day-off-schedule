2a3a7c208c4cf67bb901f77cd046d1f1baf43d74
macmini <u19b138k@wako.ac.jp>
2022-12-27 16:17:10 +0900

    add: スケジュール投稿機能追加#16

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -1,5 +1,8 @@
 class Schedule < ApplicationRecord
   belongs_to :user
   has_many :favorites
-  has_many :events
+  has_many :events, dependent: :destroy
+
+  accepts_nested_attributes_for :events, reject_if: :all_blank, allow_destroy: true
+  
 end
