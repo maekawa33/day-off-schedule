943c4fb810c35d0ddf0ad88779cf688e8cb1b44d
macmini <u19b138k@wako.ac.jp>
2022-12-27 17:45:42 +0900

    add: メールログイン機能追加#14

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'login', to: 'user_sessions#new'
+  post 'login', to: 'user_sessions#create'
+  delete 'logout', to: 'user_sessions#destroy'
+
+  resources :users, only: %i[new create]
+  
   root to: 'schedules#index'
   resources :schedules
 end
